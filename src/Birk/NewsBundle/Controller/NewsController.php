344aa63ba02c5357e8ab31bc18a708ebc4b09fb5
tetrabirk <ludovic.leyens@gmail.com>
2017-06-05 21:10:32 +0200

    hey hey hey exercie fini

--- src/Birk/NewsBundle/Controller/NewsController.php
+++ src/Birk/NewsBundle/Controller/NewsController.php
@@ -7,19 +7,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Birk\NewsBundle\Manager\NewsManager;
 
+//problemes rencontré:
+//  -je ne sais pas comment faire un lien relatif pour le css du coup j'ai du tricher et utiliser "/home" au lieu de '/'
+//  -je ne sais pas comment utiliser les routes pour générer un href
+//
+//
+
+
 class NewsController extends Controller
 {
+    private $newsManager;
+    private $news;
+
+    function __construct()
+    {
+        $this->setNewsManager();
+        $this->setNews(12);
+    }
+//    public $newsManager = new NewsManager();
+//    public $news = ;
     /**
-     * @Route("/")
+     * @Route("/home")
      * name="home"
      */
     public function homeAction()
     {
-        $newsManager = new NewsManager();
-        $news = $newsManager->create(5);
-        dump($news);
+
         $content = $this
-            ->renderView('BirkNewsBundle:News:newsAll.html.twig',['news'=>$news]);
+            ->renderView('BirkNewsBundle:News:newsAll.html.twig',['news'=>$this->news]);
         return new Response($content);
     }
     /**
@@ -29,6 +44,49 @@ class NewsController extends Controller
      */
 
     public function newsDetailAction($id=0){
+        if ($id!==0){
+            $news = $this->getNews();
+            $content = $this
+                ->renderView('BirkNewsBundle:News:newsOne.html.twig',['news'=>$news[$id]]);
+            dump($news[$id]);
+            return new Response($content);
+        }else{
+            return new Response('404');
+        }
 
     }
+
+    /**
+     * @return mixed
+     */
+    public function getNewsManager()
+    {
+        return $this->newsManager;
+    }
+
+    /**
+     * @param mixed $newsManager
+     */
+    public function setNewsManager()
+    {
+        $this->newsManager = new NewsManager();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNews()
+    {
+        return $this->news;
+    }
+
+    /**
+     * @param mixed $news
+     */
+    public function setNews($nbre)
+    {
+        $this->news = $this->newsManager->create(12);
+    }
+
+
 }
--- src/Birk/NewsBundle/Manager/NewsManager.php
+++ src/Birk/NewsBundle/Manager/NewsManager.php
@@ -11,15 +11,27 @@ use \Birk\NewsBundle\Entity\News;
 
 class NewsManager
 {
+    public $imageSrc =
+        [
+            'http://www.placecage.com/c/',
+            'http://www.placecage.com/',
+            'http://www.placecage.com/g/',
+            'http://www.placecage.com/gif/',
+            'http://www.fillmurray.com/',
+            'http://www.fillmurray.com/g/',
+            'http://www.stevensegallery.com/',
+            'http://www.stevensegallery.com/g/',
+            'http://lorempixel.com/',
+            'http://lorempixel.com/',
+        ];
 
     public function randomNews(){
         $news =[];
         $faker = \Faker\Factory::create('fr_BE');
 
-        $news['image'] = 'http://www.placecage.com/c/185/185';
+        $news['image'] = $this->randomImageSrc(185,185);
         $news['texte'] = $faker->text(200);
         $news['titre'] = $faker->text(30);
-        $news['href'] = '#';
 
         return $news;
     }
@@ -29,11 +41,20 @@ class NewsManager
         for ($i=0;$i<$n; $i++){
             $news = $this->randomNews();
             $news['id']=$i+1;
+            $news['href'] = '/news/'.$news['id'];
             $groupe[$i]= new News();
             $groupe[$i]->hydrate($news);
 
         }
-        dump($groupe);
         return $groupe;
     }
+
+    public function randomImageSrc($width,$height){
+        $imgSrcSize = sizeof($this->imageSrc);
+        $randKey = rand(0,$imgSrcSize-1);
+        $link = $this->imageSrc[$randKey].$width.'/'.$height;
+        return $link;
+
+    }
+
 }
