f0d4b54176bb1f083afbea140ac10ecc312fcc32
tetrabirk <ludovic.leyens@gmail.com>
2017-06-05 15:15:10 +0200

    pffffff

---
+++ src/Birk/NewsBundle/Entity/News.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ludovic
+ * Date: 5/06/2017
+ * Time: 14:14
+ */
+
+namespace Birk\NewsBundle\Entity;
+
+
+class News
+{
+
+}
---
+++ src/Birk/NewsBundle/Manager/NewsManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ludovic
+ * Date: 5/06/2017
+ * Time: 14:39
+ */
+
+namespace Birk\NewsBundle\Manager;
+
+
+class NewsManager
+{
+
+}
